c9a660e7985517913cd51867197f0793e9ff587e
rafael_araujo <rafafredericoaraujogcp@gmail.com>
2025-06-21 15:57:34 -0300

    chamada a api

--- datalake/Airflow/dags/dag_upload_minio.py
+++ datalake/Airflow/dags/dag_upload_minio.py
@@ -2,6 +2,7 @@ from airflow import DAG
 from airflow.providers.amazon.aws.hooks.s3 import S3Hook
 from airflow.operators.python import PythonOperator
 from datetime import datetime
+from pyspark.sql import SparkSession
 
 def upload_file_to_minio():
     s3_hook = S3Hook(aws_conn_id='minio_s3_conn') # Usa a conexão que você configurou
@@ -25,6 +26,25 @@ def download_file_from_minio():
     file_content = s3_hook.read_key(key=key, bucket_name=bucket_name)
     print(f"Content of '{key}':\n{file_content}")
 
+def leApi():
+    import requests
+    import json
+
+    url = "https://api.openbrewerydb.org/v1/breweries"  # Substitua pela URL da sua API
+    response = requests.get(url)
+
+    if response.status_code == 200:
+        data = response.json()
+        
+
+    spark = SparkSession.builder \
+        .appName("leApi") \
+        .getOrCreate()
+    df = spark.read.json(spark.sparkContext.parallelize(data))
+    df.show()
+
+
+
 with DAG(
     dag_id='dag_upload_minio',
     start_date=datetime(2023, 1, 1),
@@ -35,10 +55,9 @@ with DAG(
         task_id='upload_file',
         python_callable=upload_file_to_minio,
     )
-
-    download_task = PythonOperator(
-        task_id='download_file',
-        python_callable=download_file_from_minio,
+    leApi = PythonOperator( task_id='leApi',
+        python_callable=leApi(),
     )
 
-    upload_task >> download_task
+
+    leApi >>upload_task 
